d2e8d0b924280dc4998bdcf352e776c61d0aba6f
adam.brown <adam.brown@JBRL023>
2021-03-08 18:17:27 -0600

    moved recipe start styling layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { AppBar, Typography, Toolbar, Container, Drawer, List, ListItem } from '@material-ui/core'
 import recipes from "../recipes/index"
 import profile from "../profile/index"
 import './styles.css';
@@ -8,16 +9,37 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/profile">profile</Link>
-          </nav>
-          <Switch>
-            <Route exact path="/" component={recipes} />
-            <Route exact path="/profile" component={profile} />
-          </Switch>
+        <AppBar >
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              Wellness.com
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      <Drawer
+        variant="permanent"
+      >
+            <Switch>
+            <List>
+              <Link to="/">
+                <ListItem button >Home</ListItem>
+              </Link>
+              <Link to="/profile">
+                <ListItem button >Profile</ListItem>
+              </Link>
+              </List>
+            </Switch>
+      </Drawer>
+      <main >
+        <div className="content" >
+          <Container maxWidth="lg" >
+            <Switch>
+              <Route exact path="/" component={recipes} />
+              <Route exact path="/profile" component={profile} />
+            </Switch>
+          </Container >
         </div>
+      </main>
       </Router>
     </div>
   );
