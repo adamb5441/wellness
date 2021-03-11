e0a323b38cbe929a4dc72dcd699d93077e35a1ab
adam.brown <adam.brown@JBRL023>
2021-03-11 16:07:44 -0600

    logout and header

--- src/app/components/LoginButton.tsx
+++ src/app/components/LoginButton.tsx
@@ -1,11 +1,66 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import Button from '@material-ui/core/Button'
+import { Button, Avatar  }from '@material-ui/core'
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, user, isAuthenticated, isLoading, logout  } = useAuth0();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-  return <Button color="inherit" onClick={() => loginWithRedirect()}>Login</Button>;
+  const handleMenu = function(event: React.ChangeEvent<any>) {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    !isAuthenticated ?
+
+    <Button color="inherit" onClick={() => loginWithRedirect()}>Login</Button>
+
+    :
+
+    <div>
+    <IconButton
+      aria-label="account of current user"
+      aria-controls="menu-appbar"
+      aria-haspopup="true"
+      onClick={handleMenu}
+      color="inherit"
+    >
+      <Avatar  src={user.picture} alt={user.name}  />
+    </IconButton>
+    <Menu
+      id="menu-appbar"
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={open}
+      onClose={handleClose}
+    >
+      <MenuItem onClick={handleClose}>Profile</MenuItem>
+      <MenuItem onClick={handleClose}>My account</MenuItem>
+      <MenuItem onClick={() => {
+        logout({ returnTo: window.location.origin })
+        handleClose()
+      }}>Logout</MenuItem>
+    </Menu>
+  </div>
+  );
 
 };
 
