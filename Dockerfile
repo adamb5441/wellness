bcbdbc305f41e77180283370f4e6139c41e4fb93
adam.brown <adam.brown@JBRL023>
2021-03-15 11:14:41 -0500

    working proxy

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM node:alpine
 WORKDIR /app
+EXPOSE 80
+EXPOSE 5000
+EXPOSE 3000
 COPY package.json /app
 RUN yarn install
 COPY . /app
--- src/recipes/index.tsx
+++ src/recipes/index.tsx
@@ -3,13 +3,27 @@ import RecipesList from "./list/recipesList"
 import RecipesSearch from "./search/searchInputSection"
 import Card from '@material-ui/core/Card';
 import Divider from '@material-ui/core/Divider';
+import { useAuth0 } from "@auth0/auth0-react";
 
-
+import axios from 'axios';
 
 function Recipes() {
   
-  const searchRecipes = function(queryString: string){
+  const { getAccessTokenSilently } = useAuth0();
+  const searchRecipes = async function(queryString: string){
     console.log(queryString);
+    // const token = await getAccessTokenSilently({
+    //   audience: `https://WellnessRecipes.com`,
+    // })
+
+      axios.get(
+        `/api/Search/OpenSearch?page=1&pageSize=5`
+      ).then(res => {
+        console.log(res)
+      }).catch( err =>{
+        console.log(err)
+      })
+
   }
   return (
     <Card >
